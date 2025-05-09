8c0a87698c386e704a49d62804259a405aed034a
svnmin <svnmindsn@gmail.com>
2025-05-09 19:32:45 +0900

    starting next project

---
+++ src/api/api.jsx
@@ -0,0 +1,20 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBltJ0Xrp9Wm5dlglJ_NWvlh4bh1dAHK8M",
+  authDomain: "onlinestoreproject-77415.firebaseapp.com",
+  databaseURL: "https://onlinestoreproject-77415-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "onlinestoreproject-77415",
+  storageBucket: "onlinestoreproject-77415.firebasestorage.app",
+  messagingSenderId: "456562185387",
+  appId: "1:456562185387:web:50de631aaa63b0d0b313ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
